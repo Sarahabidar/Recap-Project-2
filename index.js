33a9e5b8dfb667520373fdf86905fa514bc9ccee
sara <sarah.abiidar@gmail.com>
2024-05-16 11:14:09 +0200

    bookmark will **toggle between both

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // phew… not a lot going on here. Please add some code!
+const bookmarkIcon = document.querySelector('[data-js="bookmark"]');
+
+bookmarkIcon.addEventListener("click", function () {
+  bookmarkIcon.classList.toggle("bookmark--active");
+});
