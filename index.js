69ad745bd9e962433f3bcb2b05aade6ec2e7eee1
sara <sarah.abiidar@gmail.com>
2024-05-16 12:34:58 +0200

    answer button with show and hidden

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const bookmarkIcon = document.querySelector('[data-js="bookmark"]');
 bookmarkIcon.addEventListener("click", function () {
   bookmarkIcon.classList.toggle("bookmark--active");
 });
+//answer Button
+const answerButton = document.querySelector('[data-js="card__button-answer"]');
+const answerOrHidden = document.querySelector('[data-js="card__answer"]');
+answerButton.addEventListener("click", function () {
+  answerOrHidden.classList.toggle("card__answer--active");
+  answerOrHidden.classList.toggle("hidden");
+  if (answerOrHidden.classList.contains("hidden")) {
+    answerButton.textContent = "Hide Answer";
+  } else {
+    answerButton.textContent = "Show Answer";
+  }
+});
